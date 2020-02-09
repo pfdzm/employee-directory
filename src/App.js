d9c348388dd84b595b370abec7fa9948a4e1b83e
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-02-09 12:08:19 +0100

    make data a prop for the entire app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+// import some randomly generated user data from https://randomuser.me/
+import { results as data } from "./users.json";
+
 import Container from "./components/Container";
 
 function App() {
   return (
     <div>
-      <Container />
+      <Container data={data} />
     </div>
   );
 }
