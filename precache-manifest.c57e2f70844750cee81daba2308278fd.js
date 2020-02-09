04297eb4f73757f2a4cdce2744c624b431e12597
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-02-09 12:17:42 +0100

    Updates

---
+++ precache-manifest.c57e2f70844750cee81daba2308278fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7684b3fbc640db7269863bc00fa3d1c5",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "3ce1bb0a66548ce3c047",
+    "url": "/employee-directory/static/css/main.419a2e72.chunk.css"
+  },
+  {
+    "revision": "6d3cb3081d51686fac7e",
+    "url": "/employee-directory/static/js/2.d5870580.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/employee-directory/static/js/2.d5870580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce1bb0a66548ce3c047",
+    "url": "/employee-directory/static/js/main.f09cd06f.chunk.js"
+  },
+  {
+    "revision": "4442c7b9b0546eb84625",
+    "url": "/employee-directory/static/js/runtime-main.46acbf45.js"
+  }
+]);
